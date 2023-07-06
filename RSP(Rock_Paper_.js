ca40d3094635c3d71997d570a87e51c774bbf315
Varad <varadraskar21@gmail.com>
2023-07-06 15:01:50 +0530

    committed to new branch

---
+++ RSP(Rock_Paper_.js
@@ -0,0 +1,13 @@
+function getComputerChoice (){
+
+}
+
+function playRound (playerSelection, computerSelection){
+
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection))
+
+//committed to branch code
